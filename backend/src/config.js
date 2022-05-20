3d115a438f29708ff601bd9e94d96d8133e93b57
wcorbani <williamcorbani@gmail.com>
2022-05-20 13:26:01 -0700

    update start date

--- backend/src/config.js
+++ backend/src/config.js
@@ -62,7 +62,7 @@ const TOKENS_PER_MINT = 20; // maximum number of NFTs a user can mint in a singl
 const PUBLIC_MINT_START_DATE = "2022-07-01T10:33:33+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-06-01T10:33:33+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-05-01T10:33:33+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0xa620D5b98AeABFe3E175BC8ad045f991768c693c"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
